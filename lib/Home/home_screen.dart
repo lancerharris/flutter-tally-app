f27dbb9379932e1add5e999c3ede5516a41cbe65
Lance Harris <lancerprogram@gmail.com>
2021-10-31 13:13:51 -0400

    Make Home screen stateless.

--- lib/Home/home_screen.dart
+++ lib/Home/home_screen.dart
@@ -3,15 +3,39 @@ import 'package:provider/provider.dart';
 import 'package:tally_app/Home/widgets/parent_task_panel.dart';
 import 'package:tally_app/providers/task_manager.dart';
 
-class HomeScreen extends StatefulWidget {
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
+
+//   @override
+//   _HomeScreenState createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   // final List<TallyPanel> _tallyPanelItems = generateTallyItems();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var parentListLength = Provider.of<TaskManager>(context).parentListLength;
+
+//     return parentListLength > 0
+//         ? ListView.separated(
+//             padding: EdgeInsets.all(8),
+//             itemCount: parentListLength,
+//             itemBuilder: (context, index) {
+//               return ParentTaskPanel(index: index);
+//             },
+//             separatorBuilder: (_, index) => SizedBox(
+//               height: 5,
+//             ),
+//           )
+//         : const Center(
+//             child: Text('Start adding tally tasks!'),
+//           );
+//   }
+// }
+
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  // final List<TallyPanel> _tallyPanelItems = generateTallyItems();
 
   @override
   Widget build(BuildContext context) {
