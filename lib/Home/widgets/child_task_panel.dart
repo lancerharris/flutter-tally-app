b2fe592afc7e6021c2fcd446d6ca1649572c68c4
Lance Harris <lancerprogram@gmail.com>
2021-11-02 09:11:33 -0400

    Move gesture detector to the Card.

--- lib/Home/widgets/child_task_panel.dart
+++ lib/Home/widgets/child_task_panel.dart
@@ -11,37 +11,36 @@ class ChildTaskPanel extends StatelessWidget {
     var childPanelItem =
         Provider.of<TaskManager>(context).getChildItemById(childItemId);
     var isExpanded = childPanelItem.isExpanded;
-    return Card(
-      child: Container(
-        padding: const EdgeInsets.all(5),
-        margin: const EdgeInsets.all(5),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(childPanelItem.name),
-                GestureDetector(
-                  child: isExpanded
+    return GestureDetector(
+      child: Card(
+        child: Container(
+          padding: const EdgeInsets.all(5),
+          margin: const EdgeInsets.all(5),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(childPanelItem.name),
+                  isExpanded
                       ? Icon(Icons.expand_less)
                       : Icon(Icons.expand_more),
-                  onTap: () {
-                    Provider.of<TaskManager>(context, listen: false)
-                        .updateExpansion(
-                      childPanelItem.id,
-                      childPanelItem.isCollection,
-                    );
-                  },
-                ),
-              ],
-            ),
-            if (isExpanded)
-              SizedBox(
-                height: 10,
+                ],
               ),
-          ],
+              if (isExpanded)
+                SizedBox(
+                  height: 10,
+                ),
+            ],
+          ),
         ),
       ),
+      onTap: () {
+        Provider.of<TaskManager>(context, listen: false).updateExpansion(
+          childPanelItem.id,
+          childPanelItem.isCollection,
+        );
+      },
     );
   }
 }
--- lib/Home/widgets/parent_task_panel.dart
+++ lib/Home/widgets/parent_task_panel.dart
@@ -14,62 +14,60 @@ class ParentTaskPanel extends StatelessWidget {
         Provider.of<TaskManager>(context).getParentItemByIndex(index);
     var isExpanded = parentListItem.isExpanded;
 
-    return Card(
-      child: Container(
-        padding: const EdgeInsets.all(5),
-        margin: const EdgeInsets.all(5),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(parentListItem.name),
-                GestureDetector(
-                  child: isExpanded
+    return GestureDetector(
+      child: Card(
+        child: Container(
+          padding: const EdgeInsets.all(5),
+          margin: const EdgeInsets.all(5),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(parentListItem.name),
+                  isExpanded
                       ? Icon(Icons.expand_less)
                       : Icon(Icons.expand_more),
-                  onTap: () {
-                    if (parentListItem.isCollection) {
-                      Provider.of<TaskManager>(context, listen: false)
-                          .updateExpansion(
-                              parentListItem.id, parentListItem.isCollection);
-                    } else {
-                      Provider.of<TaskManager>(context, listen: false)
-                          .updateExpansion(
-                        parentListItem.id,
-                        parentListItem.isCollection,
-                      );
-                    }
-                  },
-                ),
-              ],
-            ),
-            if (isExpanded)
-              SizedBox(
-                height: 10,
+                ],
               ),
-            if (parentListItem.isCollection &&
-                isExpanded &&
-                (parentListItem as TallyCollection).tallyTaskIds.length > 0)
-              ListView.separated(
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.only(left: 10),
-                    child: ChildTaskPanel(
-                      childItemId: parentListItem.tallyTaskIds[index],
-                      // childPanelItem: Provider.of<TallyTasks>(context)
-                      //     .getChildPanelById(panelListItem.id, index))
-                    ),
-                  );
-                  // child: Center(child: Text('Entry $index')));
-                },
-                separatorBuilder: (context, index) => Divider(),
-                itemCount: parentListItem.tallyTaskIds.length,
-              )
-          ],
+              if (isExpanded)
+                SizedBox(
+                  height: 10,
+                ),
+              if (parentListItem.isCollection &&
+                  isExpanded &&
+                  (parentListItem as TallyCollection).tallyTaskIds.length > 0)
+                ListView.separated(
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.only(left: 10),
+                      child: ChildTaskPanel(
+                        childItemId: parentListItem.tallyTaskIds[index],
+                        // childPanelItem: Provider.of<TallyTasks>(context)
+                        //     .getChildPanelById(panelListItem.id, index))
+                      ),
+                    );
+                    // child: Center(child: Text('Entry $index')));
+                  },
+                  separatorBuilder: (context, index) => Divider(),
+                  itemCount: parentListItem.tallyTaskIds.length,
+                )
+            ],
+          ),
         ),
       ),
+      onTap: () {
+        if (parentListItem.isCollection) {
+          Provider.of<TaskManager>(context, listen: false)
+              .updateExpansion(parentListItem.id, parentListItem.isCollection);
+        } else {
+          Provider.of<TaskManager>(context, listen: false).updateExpansion(
+            parentListItem.id,
+            parentListItem.isCollection,
+          );
+        }
+      },
     );
   }
 }
