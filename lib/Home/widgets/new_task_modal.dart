9f5150a38a68f958d130f5415645585992476967
Lance Harris <lancerprogram@gmail.com>
2021-11-06 10:09:45 -0400

    Work on new task modal.

--- lib/Home/widgets/new_task_modal.dart
+++ lib/Home/widgets/new_task_modal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tally_app/theme/app_theme.dart';
 
 class NewTaskModal extends StatelessWidget {
   @override
@@ -6,89 +7,25 @@ class NewTaskModal extends StatelessWidget {
     return Center(
       child: Column(
         children: [
-          Padding(
-            padding: EdgeInsets.only(top: 16),
-          ),
-          Text(
-            'New Tally Task',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
             ),
-          )
+            child: Container(
+              padding: EdgeInsets.all(10),
+              color: AppTheme.secondaryColor,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('New Tally Task',
+                      style: Theme.of(context).textTheme.headline6),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
-// temporary from developer quest
-
-/// This is a simple text header for the tasks list.
-class TaskPickerHeader extends SliverPersistentHeaderDelegate {
-  final String title;
-  final bool showLine;
-  const TaskPickerHeader(this.title, {this.showLine = true});
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Stack(overflow: Overflow.visible, children: [
-      showLine
-          ? Positioned.fromRect(
-              rect: Rect.fromLTWH(26, 25, 2, 35 - shrinkOffset),
-              child: SizedOverflowBox(
-                size: const Size.fromHeight(0),
-                child: Container(color: treeLineColor),
-              ),
-            )
-          : Container(),
-      Row(
-        children: <Widget>[
-          const SizedBox(width: 15),
-          Container(
-              width: 25,
-              height: 25,
-              decoration: const BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                      color: Color.fromRGBO(84, 114, 244, 0.25),
-                      offset: Offset(0, 10),
-                      blurRadius: 10,
-                      spreadRadius: 0),
-                ],
-                borderRadius: BorderRadius.all(Radius.circular(12.5)),
-                color: Color.fromRGBO(84, 114, 239, 1),
-              ),
-              child: const Icon(Icons.keyboard_arrow_down)),
-          const SizedBox(width: 15),
-          Text(
-            title,
-            style: buttonTextStyle,
-          ),
-        ],
-      ),
-    ]);
-  }
-
-  @override
-  double get maxExtent => 60;
-
-  @override
-  double get minExtent => 60;
-
-  @override
-  bool shouldRebuild(TaskPickerHeader oldDelegate) {
-    return title != oldDelegate.title;
-  }
-}
-
-const Color treeLineColor = Color.fromRGBO(215, 215, 215, 1);
-const Color contentColor = Color.fromRGBO(38, 38, 47, 1);
-
-const TextStyle buttonTextStyle = TextStyle(
-  fontFamily: 'MontserratMedium',
-  fontSize: 16,
-  color: contentColor,
-);
