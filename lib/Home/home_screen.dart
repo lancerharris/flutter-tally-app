65c87191ff3c1859b05243845b115054e3a7cf5e
Lance Harris <lancerprogram@gmail.com>
2021-11-15 14:16:25 -0500

    Make a reorderable list w/ differing sizes.

--- lib/Home/home_screen.dart
+++ lib/Home/home_screen.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tally_app/Home/models/tally_collection.dart';
 import 'package:tally_app/Home/models/tally_task.dart';
 import 'package:tally_app/Home/widgets/parent_task_panel.dart';
+import 'package:tally_app/custom_widgets/new_reorderable_list.dart';
 import 'package:tally_app/providers/task_manager.dart';
+import 'dart:ui';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -28,7 +31,10 @@ class HomeScreen extends StatelessWidget {
 
     var parentListItems = Provider.of<TaskManager>(context).parentItemList;
     return parentListItems.length > 0
-        ? ReorderableListView(
+        ? ReorderableListSizesDiffer(
+            // ? will proxyDecorator allow a shake effect
+            proxyDecorator: _proxyDecorator,
+            physics: BouncingScrollPhysics(),
             padding: EdgeInsets.all(8),
             children: [
               for (var i = 0; i < parentListItems.length; i++)
@@ -49,4 +55,20 @@ class HomeScreen extends StatelessWidget {
             child: Text('Start adding tally tasks!'),
           );
   }
+}
+
+// right now this is just dictating the animation of the elevation
+Widget _proxyDecorator(Widget child, int index, Animation<double> animation) {
+  return AnimatedBuilder(
+    animation: animation,
+    builder: (BuildContext context, Widget? child) {
+      final double animValue = Curves.easeInOut.transform(animation.value);
+      final double elevation = lerpDouble(0, 6, animValue)!;
+      return Material(
+        elevation: elevation,
+        child: child,
+      );
+    },
+    child: child,
+  );
 }
