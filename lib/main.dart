dd8af0cf1281e99a2f6c0938ac0ffd31777e7478
Lance Harris <lancerprogram@gmail.com>
2022-01-02 11:51:54 -0500

    Get hive initialized.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('tally_tasks');
+  await Hive.openBox('tally_collections');
   runApp(MyApp());
 }
