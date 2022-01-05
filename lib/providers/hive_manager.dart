9a138ad05e4475cc692309de0a38d2902b3effd7
Lance Harris <lancerprogram@gmail.com>
2022-01-05 11:23:31 -0500

    Start adding crud ops.

---
+++ lib/providers/hive_manager.dart
@@ -0,0 +1,35 @@
+import 'package:hive/hive.dart';
+import 'package:tally_app/models/tally_collection.dart';
+import 'package:tally_app/models/tally_item.dart';
+import 'package:tally_app/models/tally_task.dart';
+
+import '../models/enum_collection_status.dart';
+
+class HiveManager {
+  void createTask(TallyTask task) => Hive.box('tally_tasks').add(task);
+  void createCollection(TallyCollection collection) =>
+      Hive.box('tally_collection').add(collection);
+
+  Future<List<TallyTask>> readTallyTasks() async {
+    List<TallyTask> taskList = [];
+    final tallyTasks = await Hive.box('tally_tasks');
+    for (var i = 0; i < tallyTasks.length; i++) {
+      final tallyTask = tallyTasks.getAt(i) as TallyTask;
+      taskList.add(tallyTask);
+    }
+    return taskList;
+  }
+
+  Future<List<TallyCollection>> readTallyCollections() async {
+    List<TallyCollection> collectionList = [];
+    final tallyCollections = await Hive.box('tally_collections');
+    for (var i = 0; i < tallyCollections.length; i++) {
+      final tallyCollection = tallyCollections.getAt(i) as TallyCollection;
+      collectionList.add(tallyCollection);
+    }
+    return collectionList;
+  }
+
+  void updateTask(TallyTask task) => task.save();
+  void updateCollection(TallyCollection collection) => collection.save();
+}
